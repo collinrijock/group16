5296ac6ac4cbe663ce891dd6b7ad5597e28505a7
samantharazon <samantharazon2@gmail.com>
2021-09-13 20:53:13 -0400

    added new comment

--- src/main/java/Scrumtious/Group/Project/GroupProjectApplication.java
+++ src/main/java/Scrumtious/Group/Project/GroupProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GroupProjectApplication {
 
 	public static void main(String[] args) {
+		//sami
 		SpringApplication.run(GroupProjectApplication.class, args);
 	}
 
