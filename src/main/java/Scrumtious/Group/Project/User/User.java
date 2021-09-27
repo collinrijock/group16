dec0fd83cd404cdcc5211031f54c59280b7464d3
Tony Rey <tony_rey221@yahoo.com>
2021-09-27 15:02:09 -0400

    updated application properties and user class

--- src/main/java/Scrumtious/Group/Project/User/User.java
+++ src/main/java/Scrumtious/Group/Project/User/User.java
@@ -1,4 +1,6 @@
 package Scrumtious.Group.Project.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 
 public class User {
