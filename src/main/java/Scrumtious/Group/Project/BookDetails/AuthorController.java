cda14746a77e1a4d49f7a9317eb909f5a553d315
MurphyRiver <82214364+MurphyRiver@users.noreply.github.com>
2021-11-17 15:26:33 -0500

    Add files via upload

---
+++ src/main/java/Scrumtious/Group/Project/BookDetails/AuthorController.java
@@ -0,0 +1,46 @@
+package Scrumtious.Group.Project.BookDetails;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(path = "api")
+
+public class AuthorController {
+
+    @Autowired
+    AuthorRepository authorRepository;
+
+    @Autowired
+    BookRepository bookRepository;
+
+    // ==============================================================================
+    // Post Requests
+    // ==============================================================================
+
+    // * Checklist 3 *
+    // Create new author
+    @PostMapping("/author/create")
+    public Author createBook(@Valid @RequestBody Author author) {
+        return authorRepository.save(author);
+    }
+
+    // ==============================================================================
+    // Get Requests
+    // ==============================================================================
+
+    // * Optional *
+    // Get all authors
+    @GetMapping("/author/get")
+    public List<Author> getBooks() {
+        return authorRepository.findAll();
+    }
+
+}// end class
