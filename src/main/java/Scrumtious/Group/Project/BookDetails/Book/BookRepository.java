3c5c0eb2a5dc68b1e46a921df5a0b332660e1aa6
Samantha Victoria Rivera <samanthariver@Samanthas-MacBook-Pro.local>
2021-11-29 16:29:22 -0500

    made changes to book

--- src/main/java/Scrumtious/Group/Project/BookDetails/Book/BookRepository.java
+++ src/main/java/Scrumtious/Group/Project/BookDetails/Book/BookRepository.java
@@ -1,7 +1,5 @@
 package Scrumtious.Group.Project.BookDetails.Book;
 
-
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -9,9 +7,19 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BookRepository extends MongoRepository<Book, String> {
 
-   List<Book> findBookByIsbn(String isbn);
+    // find books by isbn
+    List<Book> findBookByIsbn(String isbn);
 
-   List<Book> findByAuthorId(String id);
+    // find books by author 
+    List<Book> findByAuthorId(String id);
+
+    // find books by genre 
+    List<Book> findByGenre(String genre);
+
+    // find books by top sellers 
+    List<Book> findTop10ByOrderByCopiesSoldDesc();
+
+    // find books by rating 
+    List<Book> findAllByRatingIsGreaterThanEqual(double rating);
 
 }
-
