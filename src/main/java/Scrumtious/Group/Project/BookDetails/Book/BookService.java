49938bb8be8eb1f9c9fe6035a004cf8df37f3e95
samantharazon <samantharazon2@gmail.com>
2021-11-29 19:24:16 -0500

    updated pom.xml file

--- src/main/java/Scrumtious/Group/Project/BookDetails/Book/BookService.java
+++ src/main/java/Scrumtious/Group/Project/BookDetails/Book/BookService.java
@@ -2,14 +2,18 @@ package Scrumtious.Group.Project.BookDetails.Book;
 
 import org.springframework.stereotype.Service;
 import java.util.List;
-
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
 @Service
 public class BookService{
+
+   @Autowired
+   BookRepository bookRepository;
 
    private MongoTemplate mongoTemplate;
 
@@ -23,4 +27,9 @@ public class BookService{
       List<Book> books = mongoTemplate.find(query,Book.class);
       return books;
    }
+
+   public Page<Book> getPages(Pageable page) {
+      return bookRepository.findAll(page);
+   }
+
 }
